d3ac9fbe915923dfe7cc0a9a73e36bcc44eda6e6
crazeidea <crazeidea@gmail.com>
2021-12-24 21:12:30 +0900

    [Feature] Update login logic

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
+import { ToastService } from 'src/app/services/toast.service';
 import { CreateUserDTO, LoginDTO } from 'src/interface/dto/auth.dto';
 
 @Component({
@@ -17,7 +19,9 @@ export class LoginPage implements OnInit {
   });
 
   constructor(
-    private authService: AuthService
+    private router: Router,
+    private authService: AuthService,
+    private toastService: ToastService
   ) { }
 
   ngOnInit() {
@@ -26,9 +30,11 @@ export class LoginPage implements OnInit {
   submit() {
     const data: LoginDTO = this.loginForm.getRawValue();
     this.authService.login(data).subscribe((res) => {
-
+      sessionStorage.setItem('accessToken', res.accessToken);
+      this.toastService.present('로그인 하였습니다.', 'light');
+      this.router.navigateByUrl('/students');
     }, (res: HttpErrorResponse) => {
-
+      this.toastService.present(res.error.message, 'danger');
     });
   }
 
